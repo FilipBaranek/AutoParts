f1ec6a54f24e95f61f2ae592649e4e6641c6d253
FilipBaranek <filipbaranek22@gmail.com>
2024-11-30 02:56:30 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.use("/styles", express.static(__dirname + "/src/public/styles"));
 app.use("/images", express.static(__dirname + "/src/views/images"));
 app.use("/views", express.static(__dirname + "/src/views"));
 app.set("view engine", "ejs");
+app.set("views", "./src/views");
 app.use(cookie());
 app.use(express.json());
 app.use("/", require("./src/routes/pages"));
